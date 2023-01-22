a044bf3ae87587f7c215f752b8a98eebb9e9d709
mat <alla0242@algonquinlive.com>
2023-01-22 09:43:36 -0500

    completed first week

--- helloStudents.js
+++ helloStudents.js
@@ -1,5 +1,15 @@
 'use strict'
 
+const students = require("./students.json");
+
+students.forEach(({ firstName, lastName}) => {console.log(`Hello ${firstName} ${lastName}`)});
+
+const studentCount = students.reduce((total, currentStudent) => {
+    if (currentStudent.lastName[0].toUpperCase() === "D") {
+        return total + 1;    }
+        return total;
+}, 0)
+
 // 1. Read the JSON file into a variable called students
 
 // 2. Iterate over the students array and print Hello with their full names to the console
